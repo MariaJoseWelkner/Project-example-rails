6c88da73f808c539bb31539b3cd67be42eccfb7d
MariaJoseWelkner <josewelkner@gmail.com>
2022-07-20 13:49:19 -0400

    agregando project

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+class PagesController < ApplicationController
+
+    def one #cada vez que queramos generar una pag en html tenemos que generar un metodo
+
+    end
+
+    def two
+
+    end
+
+    def three
+
+    end
+end
